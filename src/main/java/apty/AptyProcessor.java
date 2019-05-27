3f3dba992e9fa62741929e62413055bc138b3056
Teletha <Teletha.T@gmail.com>
2019-05-27 20:08:18 +0900

    Refactoring processor related classes.

--- src/main/java/apty/AptyProcessor.java
+++ src/main/java/apty/AptyProcessor.java
@@ -9,23 +9,30 @@
  */
 package apty;
 
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.lang.annotation.Annotation;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import javax.annotation.processing.Completion;
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic.Kind;
+import javax.tools.JavaFileObject;
 
 public abstract class AptyProcessor implements Processor {
 
@@ -36,7 +43,15 @@ public abstract class AptyProcessor implements Processor {
     protected Messager messager;
 
     /** The registered processors. */
-    private final Map<Class, Consumer<Element>> processors = new HashMap();
+    private final Map<Class<? extends Annotation>, AnnotationProcessing<Element>> processors = new HashMap();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final Set<String> getSupportedAnnotationTypes() {
+        return processors.keySet().stream().map(Class::getCanonicalName).collect(Collectors.toUnmodifiableSet());
+    }
 
     /**
      * {@inheritDoc}
@@ -74,16 +89,53 @@ public abstract class AptyProcessor implements Processor {
     }
 
     /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final boolean process(Set<? extends TypeElement> annotations, RoundEnvironment round) {
+        if (annotations.isEmpty()) {
+            return true;
+        }
+
+        processors.forEach((type, processor) -> {
+            round.getElementsAnnotatedWith(type).forEach(element -> {
+                try {
+                    processor.process(element);
+                } catch (Fail fail) {
+                    messager.printMessage(Kind.ERROR, fail.getMessage(), fail.e);
+                } catch (Exception e) {
+                    messager.printMessage(Kind.ERROR, e.getMessage(), element);
+                }
+            });
+        });
+        return true;
+    }
+
+    /**
      * Register your process by type.
      * 
      * @param <A>
      * @param annotation
      * @param yourOwnProcess
      */
-    protected final <A extends Annotation> void process(Class<A> annotation, Consumer<Element> yourOwnProcess) {
+    protected final <A extends Annotation> void process(Class<A> annotation, AnnotationProcessing<Element> yourOwnProcess) {
         Objects.requireNonNull(annotation);
         Objects.requireNonNull(yourOwnProcess);
 
         processors.put(annotation, yourOwnProcess);
+    }
+
+    /**
+     * Write new source file.
+     * 
+     * @param fqcn A fully qualified class name to write.
+     * @param writingCode Write your code.
+     * @throws Exception
+     */
+    protected final void createSourceFile(String fqcn, AnnotationProcessing<Writer> writingCode) throws Exception {
+        JavaFileObject implementationFile = filer.createSourceFile(fqcn);
+        try (Writer writer = new OutputStreamWriter(implementationFile.openOutputStream(), StandardCharsets.UTF_8)) {
+            writingCode.process(writer);
+        }
     }
 }
--- src/main/java/icy/manipulator/IcyManipulator.java
+++ src/main/java/icy/manipulator/IcyManipulator.java
@@ -9,56 +9,22 @@
  */
 package icy.manipulator;
 
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
-import java.util.Set;
-
-import javax.annotation.processing.RoundEnvironment;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic.Kind;
-import javax.tools.JavaFileObject;
-
 import apty.AptyProcessor;
-import apty.Fail;
 import icy.manipulator.model.ModelDefinition;
 
 public class IcyManipulator extends AptyProcessor {
 
     /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Set<String> getSupportedAnnotationTypes() {
-        return Set.of(Icy.class.getName());
-    }
+    * 
+    */
+    public IcyManipulator() {
+        process(Icy.class, element -> {
+            ModelDefinition model = new ModelDefinition(element);
+            CodeGenerator generator = new CodeGenerator(model);
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment round) {
-        if (annotations.isEmpty()) {
-            return true;
-        }
-
-        for (Element element : round.getElementsAnnotatedWith(Icy.class)) {
-            try {
-                ModelDefinition model = new ModelDefinition(element);
-                CodeGenerator generator = new CodeGenerator(model);
-
-                JavaFileObject implementationFile = filer.createSourceFile(model.implType.toString());
-                try (Writer writer = new OutputStreamWriter(implementationFile.openOutputStream(), StandardCharsets.UTF_8)) {
-                    writer.write(generator.generate());
-                }
-            } catch (Fail fail) {
-                messager.printMessage(Kind.ERROR, fail.getMessage(), fail.e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return true;
+            createSourceFile(model.implType.toString(), writer -> {
+                writer.write(generator.generate());
+            });
+        });
     }
 }
