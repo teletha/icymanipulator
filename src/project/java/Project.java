62b1876610bc821d5b23c3db646bba6b12e24947
Teletha <Teletha.T@gmail.com>
2020-12-25 09:29:59 +0900

    Version up.

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -10,7 +10,7 @@
 public class Project extends bee.api.Project {
 
     {
-        product("com.github.teletha", "icymanipulator", "1.1.1");
+        product("com.github.teletha", "icymanipulator", "1.2.0");
 
         require("com.github.teletha", "antibug").atTest();
         require("com.github.teletha", "bee", "0.6.0").atTest();
